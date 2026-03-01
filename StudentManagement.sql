6918973b6c3ffbc9bc612274ceebf4077f21344a
khangvo95 <khang.vo@getoutabox.com>
2026-03-01 12:27:58 +0700

    homework student management

--- StudentManagement.sql
+++ StudentManagement.sql
@@ -60,7 +60,26 @@ VALUES (1, 1, 8, 1),
  (1, 2, 10, 2),
  (2, 1, 12, 1);
 
+// Hiển thị danh sách tất cả các học viên
+select * from student;
 
+// Hiển thị danh sách các học viên đang theo học.
+select * from student where status = true;
+
+// Hiển thị danh sách các môn học có thời gian học nhỏ hơn 10 giờ.
+select * from subject where Credit < 10;
+
+// Hiển thị danh sách học viên lớp A1
+select S.StudentId, S.StudentName, C.ClassName 
+from student S join class C 
+on S.ClassID = C.ClassID
+where C.ClassName = 'A1';
+
+// Hiển thị điểm môn CF của các học viên.
+SELECT S.StudentId, S.StudentName, Sub.SubName, M.Mark
+FROM Student S join Mark M on S.StudentId = M.StudentId 
+join Subject Sub on M.SubId = Sub.SubId
+WHERE Sub.SubName = 'CF';
 
 
 
